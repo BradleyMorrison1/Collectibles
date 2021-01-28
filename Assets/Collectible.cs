67d100b26684e2760befeb778e145da338bd32e8
BradleyMorrison1 <70822639+BradleyMorrison1@users.noreply.github.com>
2021-01-28 14:35:15 -0400

    Added Collectible and timer for respawn

---
+++ Assets/Collectible.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    // Variables 
+    public float timeUntilRespawn;
+    public int points;
+    // Methods
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            points++;
+            gameObject.SetActive(false);
+            Invoke("enableCollectible", timeUntilRespawn);
+        }
+    }
+
+    private void enableCollectible()
+    {
+        gameObject.SetActive(true);
+    }
+
+    private void Update() // For Rotating Collectible
+    {
+        gameObject.transform.Rotate(Vector3.forward);
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    /*
+    public GameObject collectible;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("collectible"))
+        {
+            collectible.SetActive(false);
+            Invoke("enableCollectible", 3f);
+        }
+    }
+
+    private void enableCollectible()
+    {
+            collectible.SetActive(true);
+    }
+    */
+}
